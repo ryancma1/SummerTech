e99e949980f1becad883cd4511c33c995b293d2d
ryancma1 <ryancma824@gmail.com>
2025-07-07 14:01:53 +0000

    7/7/25 - Classtime1

---
+++ counting.java
@@ -0,0 +1,9 @@
+public class counting {
+    public static void main (String args[]){
+        int n = 50;
+        while (n >= 1){
+            System.out.println(n);
+            n = n - 1;
+        }
+    }
+}
---
+++ forLoops.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class forLoops {
+    public static void main(String[] args) {
+        for (int i = 100; i <= 200; i++){
+            if (i % 4 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
