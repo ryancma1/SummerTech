e0480c3c549f4b7007003f106bd68eeedffafde6
ryancma1 <ryancma824@gmail.com>
2025-07-10 14:01:12 +0000

    7/10/25 - Classtime1

--- binarySearch.java
+++ binarySearch.java
@@ -1,36 +1,50 @@
 import java.util.Scanner;
 public class binarySearch {
-    public static void sigma(int [] numbers){
+    public static void sigma(int [] numbers, int listLength){
         int index = 0;
-        for (int i = 0; i <= 14; i++){
+        System.out.println("Here is your list: ");
+        for (int i = 0; i < listLength; i++){
             System.out.print(numbers[i] + ", ");
         }
         System.out.println("");
     }
-    public static int check(int [] numbers, int choice, int a, int b){
+    public static int check(int [] numbers, int choice, int a, int b, int listLength){
+        int index = 0;
         int splitindex = (b + a)/2;
-        if (choice > numbers[splitindex]){
+        if (choice > numbers[listLength - 2]){
+            index = listLength - 1;
+        }
+        else if (choice > numbers[splitindex]){
             a = splitindex;
-            check(numbers, choice, a, b);
+            index = check(numbers, choice, a, b, listLength);
         }
         else if (choice < numbers[splitindex]){
             b = splitindex;
-            check(numbers, choice, a, b);
+            index = check(numbers, choice, a, b, listLength);
         }
-        int index = splitindex;
+        else if (choice == numbers[splitindex]){
+            index = splitindex;
+        }
         return index;
     }
     public static void main(String[] args) {
         Scanner answer = new Scanner(System.in);
+        System.out.println("Welcome to the Binary Searcher!");
+        System.out.println("How long would you like to create your list?");
+        int listLength = answer.nextInt();
         int a = 0;
-        int b = 14;
-        int [] numbers = new int[]{1, 2, 4, 5, 12, 26, 38, 68, 174, 245, 309, 351, 440, 678, 999};
-        System.out.println("What number in this list would you like to find the index of?");
-        sigma(numbers);
-        int choice = answer.nextInt();
+        int b = listLength - 1;
         int index = 0;
-        int splitindex = 0;
-        index = check(numbers, choice, a, b);
+        int [] numbers = new int[listLength];
+        System.out.println("Please enter " + listLength + " numbers from least to greatest.");
+        System.out.println("Add spaces between every number to create the list.");
+        for (int i = 0; i < listLength; i++){
+            numbers[i] = answer.nextInt();
+        }
+        sigma(numbers, listLength);
+        System.out.println("What number in your list would you like to find the index of?");
+        int choice = answer.nextInt();
+        index = check(numbers, choice, a, b, listLength);
         System.out.println("The index of " + choice + " is " + index + ".");
         
 
