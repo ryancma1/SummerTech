e4d6be76c60d643c37bba6be1abe9edbb7cdd48d
ryancma1 <ryancma824@gmail.com>
2025-07-14 17:31:05 +0000

    7/14/25 - Classtime3

--- mergesort.java
+++ mergesort.java
@@ -6,6 +6,19 @@ public class mergesort {
         }
         System.out.println("");
     }
+    public static int[] split(int [] list, int length){
+        int start = 0;
+        int end = length - 1;
+        int [] list1 = new int[length];
+        for (int i = start; i < (end/2) - 1; i++){
+            list1 [i] = list[i];
+        }
+        for (int i = end/2; i < end; i++){
+            list1 [i] = list[i];
+        }
+        
+        
+    }
     public static void main(String[] args) {
         int length = 8;
         int range = 8;
@@ -19,36 +32,6 @@ public class mergesort {
         }
         System.out.println("Start List: ");
         print(list, length);
-        for (int i = 0; i < length; i += 2){
-            if (list[i] > list[i + 1]){
-                holder = list[i + 1];
-                list[i + 1] = list[i];
-                list[i] = holder;
-                System.out.println("Changing list: ");
-                print(list, length);
-                counter += 1;
-            }
-        }
-        for (int i = 0; i < 3; i += 1){
-            if (list[i] > list[i + 1]){
-                holder = list[i + 1];
-                list[i + 1] = list[i];
-                list[i] = holder;
-                System.out.println("Changing list: ");
-                print(list, length);
-                counter += 1;
-            }
-        }
-        for (int i = 4; i < 7; i += 1){
-            if (list[i] > list[i + 1]){
-                holder = list[i + 1];
-                list[i + 1] = list[i];
-                list[i] = holder;
-                System.out.println("Changing list: ");
-                print(list, length);
-                counter += 1;
-            }
-        }
         System.out.println("End List: ");
         print(list, length);
         System.out.println("The list changed " + counter + " times.");
