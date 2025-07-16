35dd893658c038ec26f2065d9726a8c9b8511c9d
ryancma1 <ryancma824@gmail.com>
2025-07-16 14:00:52 +0000

    7/16/25 - Classtime1

--- mergesort.java
+++ mergesort.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-import java.util.*;
 public class mergesort {
     public static void print(int [] list){
         for (int i = 0; i < list.length; i++){
@@ -18,8 +17,8 @@ public class mergesort {
         }
         return merge(list1, list2);
     }
-    public static int[] split(int [] list, int start, int end){
-        int [] list3 = new int[end-start];
+    public static int[] split(int [] list){
+        int [] list3 = new int[list.length - ];
         for (int i = start; i < end; i++){    
             list3 [i-start] = list[i];
         }
@@ -33,75 +32,63 @@ public class mergesort {
             System.out.println("i " + i);
             System.out.println("1: " + list1track);
             System.out.println("2: " + list2track);
-            if (list1track < list1.length && list2track < list2.length && list1[list1track] >= list2[list2track]){
-                list4 [i] = list2[list2track];
-                list2track += 1;
-                for (int j = 0; j < list4.length; j++){
-                    System.out.print(list4[j] + ", ");
+            if (list1track < list1.length && list2track < list2.length){
+                if(list1[list1track] > list2[list2track]){
+                    list4[i] = list2[list2track];
+                    list2track++;
+                    System.out.println("List 1: ");
+                    print(list1);
+                    System.out.println("List 2: ");
+                    print(list2);
+                    System.out.println("List 4: ");
+                    print(list4);
                 }
-                System.out.println("");
-                System.out.println("list2");  
-            }
-            else if (list1track < list1.length && list2track < list2.length && list1[list1track] <= list2[list2track]){
-                list4 [i] = list1[list1track];
-                list1track += 1;
-                for (int j = 0; j < list4.length; j++){
-                    System.out.print(list4[j] + ", ");
+                else if(list1[list1track] < list2[list2track]){
+                    list4[i] = list1[list1track];
+                    list1track++;
+                    System.out.println("List 1: ");
+                    print(list1);
+                    System.out.println("List 2: ");
+                    print(list2);
+                    System.out.println("List 4: ");
+                    print(list4);
                 }
-                System.out.println("");
-                System.out.println("list1");
-            }
-            else if (list2track >= list2.length && list1track < list1.length){
-                list4 [i] = list1[list1track];
-                list1track += 1;
-                for (int j = 0; j < list4.length; j++){
-                    System.out.print(list4[j] + ", ");
-                }
-                System.out.println("");
-                System.out.println("list1");
             }
             else if (list1track >= list1.length && list2track < list2.length){
-                list4 [i] = list2[list2track];
-                list2track += 1;
-                for (int j = 0; j < list4.length; j++){
-                    System.out.print(list4[j] + ", ");
-                }
-                System.out.println("");
-                System.out.println("list2");
+                list4[i] = list2[list2track];
+                list2track++;
+                System.out.println("List 1: ");
+                print(list1);
+                System.out.println("List 2: ");
+                print(list2);
+                System.out.println("List 4: ");
+                print(list4);
             }
-            System.out.println("forloop");
-        }    
+            else if (list1track < list1.length && list2track >= list2.length){
+                list4[i] = list1[list1track];
+                list1track++;
+                System.out.println("List 1: ");
+                print(list1);
+                System.out.println("List 2: ");
+                print(list2);
+                System.out.println("List 4: ");
+                print(list4);
+            }
+        }
         return list4;
     }
     public static void main(String[] args) {
-        int range = 20;
-        int counter = 0;
-        int holder = 0;
-        int [] list = new int[10];
-        int [] list1 = new int[list.length/2];
-        int [] list2 = new int[list.length/2];
+        int range = 10;
+        int [] list = new int[8];
         Random number = new Random();
         for (int i = 0; i < list.length; i++){
             int numberGen = number.nextInt(range);
             list[i] = numberGen;
         }
-        for (int i = 0; i < list1.length; i++){
-            int numberGen = number.nextInt(range);
-            list1[i] = numberGen;
-        }
-        for (int i = 0; i < list2.length; i++){
-            int numberGen = number.nextInt(range);
-            list2[i] = numberGen;
-        }
-        list = merge(list1, list2);
-        for (int i = 0; i < list.length; i++){
-            System.out.print(list[i] + ", ");
-        }
-        System.out.println("");
-            /*System.out.println("Start List: ");
-            print(list);
-            System.out.println("End List: ");
-            print(list);
-            System.out.println("The list changed " + counter + " times.");*/
+        System.out.println("Unsorted List: ");
+        print(list);
+        list = sort(list);
+        System.out.println("Sorted List: ");
+        print(list);
     }
 }
