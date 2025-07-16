cb965a86cb61c8aa13b8b8bde84762dcb035450b
ryancma1 <ryancma824@gmail.com>
2025-07-16 18:01:47 +0000

    7/16/25 - Classtime3

---
+++ bogoSort.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class bogoSort {
+    public static void print(int [] list){
+        for (int i = 0; i < list.length; i++){
+            System.out.print(list[i] + ", ");
+        }
+        System.out.println("");
+    }
+    public static boolean check(int [] list){
+        boolean sorted = true;
+        for (int i = 0; i < list.length - 1; i++){
+            if (list[i] > list[i+1]){
+                sorted = false;
+            }
+        }
+        return sorted;
+    }
+    public static int [] shuffle(int[] list){
+        Random number = new Random();
+        int holder = 0;
+        int random1 = number.nextInt(list.length);
+        int random2 = number.nextInt(list.length);
+        if (random1 != random2){   
+            holder = list[random1];
+            list[random1] = list[random2];
+            list[random2] = holder;
+        }
+        else{
+            shuffle(list);
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        long initialtime = System.currentTimeMillis();
+        Random number = new Random();
+        int counter = 0;
+        boolean sorted = false;
+        int [] list = new int[100];
+        for (int i = 0; i < list.length; i++){
+            int numberGen = number.nextInt(list.length);
+            list[i] = numberGen;
+        }
+        System.out.println("Original List: ");
+        print(list);
+        while(sorted == false){
+            for (int i = 0; i < list.length; i++){
+                list = shuffle(list);
+            }
+            sorted = check(list);
+            counter++;
+        }
+        long endtime = System.currentTimeMillis();
+        long time = endtime - initialtime;
+        System.out.println("Sorted List: ");
+        print(list);
+        System.out.println("The list is sorted.");
+        System.out.println("It shuffled it " + counter + " times.");
+        System.out.println("Time: " + time);
+    }
+}
--- checklist.java
+++ checklist.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class checklist {
-    public static void check(int [] list){
-        for (int i = 0; i < list.length; i++){
+    public static boolean check(int [] list){
+        boolean sorted = true;
+        for (int i = 0; i < list.length - 1; i++){
             if (list[i] > list[i+1]){
-                
+                sorted = false;
             }
         }
+        return sorted;
     }
     public static void main(String[] args) {
         Scanner answer = new Scanner(System.in);
@@ -13,6 +15,17 @@ public class checklist {
         System.out.println("Please input a list of 10 numbers");
         for (int i = 0; i < list.length; i++){
             list [i] = answer.nextInt();
+        }
+        System.out.println("List: ");
+        for (int i = 0; i < list.length; i++){
+            System.out.print(list[i] + ", ");
+        }
+        System.out.println("");
+        if (check(list)){
+            System.out.println("The list is sorted");
+        }
+        else {
+            System.out.println("The list is not sorted");
         }
 
     }
