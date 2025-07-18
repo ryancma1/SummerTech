1a4a42628e6de5e802229e3a85860b2a45b176d5
ryancma1 <ryancma824@gmail.com>
2025-07-18 15:00:12 +0000

    7/18/25 - Working on Sudoku

---
+++ Sudoku.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Sudoku{
+    public static void printEmpty(String [][] board){
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                board [i][j] = " _ ";
+            }
+        }
+        for (int i = 0; i < 9; i++){
+                for (int j = 0; j < 9; j++){
+                    System.out.print(board[i][j]);
+                }
+                System.out.println("");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner answer = new Scanner(System.in);
+        String [][] board = new String[9][9];
+        printEmpty(board);
+
+
+    }
+}
